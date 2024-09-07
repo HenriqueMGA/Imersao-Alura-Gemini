2c05ee8a600e301de07ea8094c8b86577faef874
HenriqueMGA <riquemamaral@gmail.com>
2024-09-06 22:21:42 -0300

    primeiro commit

---
+++ dados.js
@@ -0,0 +1,68 @@
+let dados = [
+    {
+        titulo: "Monkey D. Luffy",
+        descricao: "Luffy, conhecido como Chapéu de Palha, é o protagonista da obra e o capitão do bando dos Chapéus de Palha. O mesmo possui o poder da Gomu-Gomu no mi (fruta da borracha). Seu sonho é ser o rei dos piratas e encontrar o One Piece.",
+        link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy",
+        tags: "capitão protagonista D. gomu borracha"
+    },
+    {
+        titulo: "Roronoa Zoro",
+        descricao: "Zoro, conhecido como caçador de piratas ou Rei do Inferno é o espadachim do bando e é visto como o braço direito de Luffy. O mesmo usa três espadas em combate. Seu sonho é ser o melhor espadachim do mundo.",
+        link: "https://onepiece.fandom.com/pt/wiki/Roronoa_Zoro",
+        tags: "espada espadachim braço direito três espadas"
+    },
+    {
+        titulo: "Vinsmoke Sanji",
+        descricao: "Sanji, conhecido como Perna Preta, é o cozinheiro do bando e é visto como o braço esquerdo de Luffy. O mesmo usa as pernas para lutar. Seu sonho é encontrar o All Blue, mas que reune todos os peixes do mundo.",
+        link: "https://onepiece.fandom.com/pt/wiki/Sanji",
+        tags: "perna preta cozinheiro cozinha braco esquerdo"
+    },
+    {
+        titulo: "Nami",
+        descricao: "Nami, conhecida como Gata Ladra, é a navegadora do bando. A mesmo usa durante as lutas um bastão tecnológico que controla o clima. Seu sonho é criar um mapa-mundi",
+        link: "https://onepiece.fandom.com/pt/wiki/Nami",
+        tags: "zeus navegação navegadora dinheiro ladra bastão clima"
+    },
+    {
+        titulo: "Usopp",
+        descricao: "Usopp, conhecido como God Usopp ou SogeKing, é o atirador do bando possuindo uma mira inpecável mesmo sendo um dos mais fracos. O mesmo usa um estilingue com munições diferenciadas para lutar. Seu sonho é se tornar um bravo guerreiro do mar.",
+        link: "https://onepiece.fandom.com/pt/wiki/Usopp",
+        tags: "atirador atirar estilingue deus god mira"
+    },
+    {
+        titulo: "Tony Tony Chopper",
+        descricao: "Chopper é uma rena e o médico do bando. Um dos mais fracos do bando. O mesmo usa as transformações da sua Hito-Hito no mi (fruta do humano) para lutar. Seu sonho é encontrar a cura de todas as doenças do mundo.",
+        link: "https://onepiece.fandom.com/pt/wiki/Tony_Tony_Chopper",
+        tags: "médico rena tranformação hito"
+    },
+    {
+        titulo: "Nico Robin",
+        descricao: "Robin, conhecida como Criança Demônio, é a arqueóloga do bando e era uma inimiga antes de se juntar ao grupo. A mesma usa partes do seu corpo duplicadas pela sua Hana-Hana no mi (fruta da flor) em combate. Seu sonho é descobrir a história do Século Perdido.",
+        link: "https://onepiece.fandom.com/pt/wiki/Nico_Robin",
+        tags: "flor braço perna arqueologa arqueologia demônio"
+    },
+    {
+        titulo: "Cutty Flam",
+        descricao: "Cutty Flam, conhecido como Franky, é um ciborgue e o carpinteiro do bando e era um inimigo antes de se juntar ao grupo. O mesmo usa suas invenções tecnológicas no combate. Seu sonho é criar e dirigir um navio que dê a volta no mundo.",
+        link: "https://onepiece.fandom.com/pt/wiki/Franky",
+        tags: " franky Franky robo robô ciborgue carpinteiro maquina máquina"
+    },
+    {
+        titulo: "Brook",
+        descricao: "Brook, conhecido como Soul King, é um esqueleto e o músico do bando. O mesmo usa a música para controlar almas a partir da Yomi-Yomi no mi(fruta da ressurreição) e também uma espada em combate. Seu objetivo é dar a volta no mundo e reencontrar uma velha amiga.",
+        link: "https://onepiece.fandom.com/pt/wiki/Brook",
+        tags: "música musica soul king violino guitarra esqueleto yomi alma binks no sake"
+    },
+    {
+        titulo: "Jimbe",
+        descricao: "Jimbe, conhecido como Cavaleiro do Mar, é um homem-peixe da espécie tubarão-baleia e o timoneiro do bando. O mesmo usa suas técnicas de Karate dos homens-peixe para lutar. Seu sonho é fazer com que todos os humanos e homens-peixe possam coexistir.",
+        link: "https://onepiece.fandom.com/pt/wiki/Jinbe",
+        tags: "karate timoneiro tubarao baleia peixe mar"
+    },
+    {
+        titulo: "Nefertari D. Vivi",
+        descricao: "Vivi, conhecida como Princesa de Alabasta, apesar de não estar com os outros nas aventuras, é considerada por muitos como sendo do bando. A mesma uma arma feita de pequenos discos afiados ligados por um fio a seus dedos para lutar.",
+        link: "https://onepiece.fandom.com/pt/wiki/Nefertari_Vivi",
+        tags: "princesa alabasta D."
+    },
+];
